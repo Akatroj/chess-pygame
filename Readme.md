882a38e4a783538cfaf778ba7bf406fdaea38cf5
Akatroj <yjkjgdjfhjrdhgjh@gmail.com>
2021-05-01 18:45:44 +0200

    added demo animations

--- Readme.md
+++ Readme.md
@@ -1,3 +1,5 @@
 ## Python Chess 
 
 ## Made by [Akatroj](https://github.com/Akatroj) and [mario11-wiet](https://github.com/mario11-wiet)
+
+
--- src/Game/AI/ai.py
+++ src/Game/AI/ai.py
@@ -23,93 +23,67 @@ class AI:
         for row in self.board.board_arr:
             for piece in row:
                 if piece is not None and piece.color == self.board.current_player:
-                    capture, move = self.board.get_legal_moves(piece, True)
+                    move, capture = self.board.get_legal_moves(piece, True)
                     array = capture + move
-                    for i in array:
-                        # the prevention of the same movement 3rd time
-                        if AI.repeated_move(self, piece, i):
+                    for x, y in array:
+                        # prevent making the same move 3 times
+                        if self.repeated_move(piece, [x, y]):
                             continue
-                        # making movements
+                        # making moves
                         old_position = piece.get_position()
-                        piece_none = self.board.fake_move(piece, i)
+                        piece_none = self.board.fake_move(piece, [x, y])
                         # algorithm invocation alpha-beta
-                        val = AI.mini_max(self, depth + 1, not is_black_turn, -10000, 10000)
+                        val = self.mini_max(depth + 1, not is_black_turn, -10000, 10000)
                         # values for moves with a hidden bonus
-                        if type(piece) == Pawn and (i[1] == 7 or i[1] == 0):  # promotion
+                        if type(piece) == Pawn and (y == 7 or y == 0):  # promotion
                             val += 80
-                        elif self.board.is_move_en_passant(piece, i):  # en passant
+                        elif self.board.is_move_en_passant(piece, [x, y]):  # en passant
                             val += 10
                         if not is_black_turn:
                             val = - val
                         if val >= best and is_black_turn:
                             best = val
-                            best_move = i
+                            best_move = [x, y]
                             piece_ = piece
                         # undo moves
                         if piece_none is None:
                             self.board.fake_move(piece, old_position)
                         else:
                             self.board.fake_move(piece, old_position)
-                            self.board.fake_move(piece_none, i)
+                            self.board.fake_move(piece_none, [x, y])
         return piece_, best_move
 
     # mini-max algorithm with alpha-beta pruning
     def mini_max(self, depth, is_black_turn, alpha, beta):
         if self.depth == depth:
-            return -AI.evaluate_board(self)
+            return -self.evaluate_board()
         if is_black_turn:
-            arr = []
-            arr_better = []
             best = -9999
-            for row in self.board.board_arr:
-                for piece in row:
-                    if piece is not None and piece.color == 'b':
-                        arr_temporary, arr_better_temporary = AI.sort_legal_move(self, piece, 7)
-                        arr += arr_temporary
-                        arr_better += arr_better_temporary
-            arr_better.sort(key=lambda key: key[0])
-            arr = arr + arr_better
+            arr = self.possible_move('b', 7)
             for po in range(len(arr) - 1, -1, -1):
                 piece = arr[po][1]
                 i = arr[po][2]
                 old_position = piece.get_position()
                 piece_none = self.board.fake_move(piece, i)
-                val = AI.mini_max(self, depth + 1, False, alpha, beta)
+                val = self.mini_max(depth + 1, False, alpha, beta)
                 best = max(best, val)
                 alpha = max(alpha, best)
-                if piece_none is None:
-                    self.board.fake_move(piece, old_position)
-                elif piece_none is not None:
-                    self.board.fake_move(piece, old_position)
-                    self.board.fake_move(piece_none, i)
+                self.revert_move(piece_none, piece, old_position, i)
                 if beta <= alpha:
                     return best
             return best
         else:
-            arr = []
-            arr_better = []
             best = 9999
-            for row in self.board.board_arr:
-                for piece in row:
-                    if piece is not None and piece.color == 'w':
-                        arr_temporary, arr_better_temporary = AI.sort_legal_move(self, piece, 0)
-                        arr += arr_temporary
-                        arr_better += arr_better_temporary
-            arr_better.sort(key=lambda key: key[0])
-            arr = arr + arr_better
+            arr = self.possible_move('w', 0)
             for po in range(len(arr) - 1, -1, -1):
                 piece = arr[po][1]
                 i = arr[po][2]
                 old_position = piece.get_position()
                 piece_none = self.board.fake_move(piece, i)
-                val = AI.mini_max(self, depth + 1, True, alpha, beta)
+                val = self.mini_max(depth + 1, True, alpha, beta)
                 best = min(best, val)
                 beta = min(beta, best)
-                if piece_none is None:
-                    self.board.fake_move(piece, old_position)
-                elif piece_none is not None:
-                    self.board.fake_move(piece, old_position)
-                    self.board.fake_move(piece_none, i)
+                self.revert_move(piece_none, piece, old_position, i)
                 if beta <= alpha:
                     return best
             return best
@@ -134,24 +108,41 @@ class AI:
             return True
         return False
 
-    # possible moves for the piece
-    def sort_legal_move(self, piece, position):
-        # czemu ta funkcja ma w nazwie sort jezeli nic nie sortuje????
+    # add higher priority to good moves (pawn promotion, pawn capture)
+    def move_priority(self, piece, position):
         arr_better = []
         arr = []
-        move, capture = self.board.get_legal_moves(piece, True)
-        # !unpack tuple in loop
-        for i in capture:
-            if self.board.board_arr[i[0]][i[1]] is not None:
-                arr_better.append([10 * self.board.board_arr[i[0]][i[1]].points - piece.points, piece, i])
-                if type(piece) == Pawn and (i[1] == position):
+        move_arr, capture_arr = self.board.get_legal_moves(piece, True)
+        for x, y in capture_arr:
+            if self.board.board_arr[x][y] is not None:
+                arr_better.append([10 * self.board.board_arr[x][y].points - piece.points, piece, [x, y]])
+                if type(piece) == Pawn and (y == position):
                     arr_better[-1][0] = arr_better[-1][0] + 90
             else:
-                arr_better.append([piece.points, piece, i])
-        for i in move:
-            if type(piece) == Pawn and (i[1] == position):
+                arr_better.append([piece.points, piece, [x, y]])
+        for i in move_arr:
+            if type(piece) == Pawn and (i == position):
                 arr_better.append([90, piece, i])
             else:
                 arr.append([0, piece, i])
         return arr, arr_better
 
+    def possible_move(self, color, position):
+        arr = []
+        arr_better = []
+        for row in self.board.board_arr:
+            for piece in row:
+                if piece is not None and piece.color == color:
+                    arr_temporary, arr_better_temporary = self.move_priority(piece, position)
+                    arr += arr_temporary
+                    arr_better += arr_better_temporary
+        arr_better.sort(key=lambda key: key[0])
+        arr = arr + arr_better
+        return arr
+
+    def revert_move(self, piece_none, piece, old_position, i):
+        if piece_none is None:
+            self.board.fake_move(piece, old_position)
+        elif piece_none is not None:
+            self.board.fake_move(piece, old_position)
+            self.board.fake_move(piece_none, i)
